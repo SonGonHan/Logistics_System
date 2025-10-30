d8141ff44ed50a0a130f61136b6a148d99bb5f2f
SonGonHan <miskasabancik@gmail.com>
2025-10-30 20:41:24 +0300

    first commit

---
+++ UserAuthService/src/main/java/userauthservice/UserAuthServiceApplication.java
@@ -0,0 +1,15 @@
+package userauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
+
+@Modulith
+@SpringBootApplication
+public class UserAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthServiceApplication.class, args);
+    }
+
+}
