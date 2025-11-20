6e1b1f6b834009bf30724fffa6bd002a08242679
SonGonHan <miskasabancik@gmail.com>
2025-11-20 20:30:50 +0300

    Рабочее тестирование с БД

--- shared-library/src/test/java/com/logistics/shared/IntegrationTest.java
+++ shared-library/src/test/java/com/logistics/shared/IntegrationTest.java
@@ -1,6 +1,7 @@
 package com.logistics.shared;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.lang.annotation.ElementType;
@@ -12,5 +13,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
+@ActiveProfiles("test")
 public @interface IntegrationTest {
 }
